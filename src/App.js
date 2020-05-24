70fe329cd28e8ffc25ab1ae26ad5b9d7d07ca831
ozovadia94 <ozovad@gmail.com>
2020-05-24 10:26:48 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import './App.css';
+//import logo from './img/logo.jpg';
+import HomePage from './HomePage/HomePage.js';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {Link} from "react-router-dom";
+import MenuPage from './MenuPage/MenuPage';
+import Events from './Events/Events';
+import Information from './Information/Information';
+import ShiftDashboard from './Shift/ShiftDashboard';
+import CoordinatorDashboard from './Coordinator/CoordinatorDashboard';
+import PatientDashboard from './Patient/PatientDashboard';
+import EditEventsPage from './EditEventsPage/EditEventsPage';
+import VolunteerDashboard from './Volunteer/VolunteerDashboard';
+import VolunteerRequestDashboard from './VolunteerRequest/VolunteerRequestDashboard';
+import Join from './Join/Join';
+import Statistics from './Statistics/Statistics';
+
+const style = {
+  position: "relative",
+  margin: "50px auto"
+}
+
+
+class App extends Component {
+
+  onDayClick = (e, day) => {
+    alert('כמות משמרות');
+
+  }
+
+  
+  render() {
+    
+    return (
+     
+      <BrowserRouter>
+       
+      <div className="App">
+      
+      
+      <Switch>
+      <Route path="/HomePage" component={HomePage} /> 
+      <Route path="/Information" component={Information} /> 
+      <Route path="/EditEventsPage" component={EditEventsPage} /> 
+      <Route path="/ShiftDashboard" component={ShiftDashboard} />
+      <Route path="/PatientDashboard" component={PatientDashboard} />
+      <Route path="/VolunteerDashboard" component={VolunteerDashboard} />
+      <Route path="/CoordinatorDashboard" component={CoordinatorDashboard} />              
+      <Route path="/VolunteerRequestDashboard" component={VolunteerRequestDashboard} />              
+      <Route path="/Join" component={Join} /> 
+      <Route path="/Statistics" component={Statistics} />  
+      <Route path="/Events" component={Events} /> 
+      <Route path="/MenuPage" component={MenuPage} />
+      
+        
+      </Switch>
+        
+      </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+
+export default App;
+
